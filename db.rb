e9ff16d9f69cd246652f97b00cd396fa960809bf
Geoffroy Carrier <geoffroy.carrier@koon.fr>
2009-08-24 01:01:08 +0200

    Nice heroku support

--- db.rb
+++ db.rb
@@ -1,13 +1,16 @@
 require 'dm-core'
 require 'dm-timestamps'
 
-Env = ENV['RAILS_ENV'] || ENV['MERB_ENV'] || ENV['RACK_ENV'] || 'development'
-
-case Env
-when 'development'
-  DataMapper::Logger.new(STDOUT, :debug)
-  DataMapper.setup(:default, "sqlite3:///#{Dir.pwd}/db/dev.db")
+if ENV['DATABASE_URL']
+  DataMapper.setup(ENV['DATABASE_URL'])
 else
-  DataMapper::Logger.new(STDOUT, :warn)
-  DataMapper.setup(:default, "sqlite3:///#{Dir.pwd}/db/prod.db")
+  Env = ENV['RAILS_ENV'] || ENV['MERB_ENV'] || ENV['RACK_ENV'] || 'development'
+  case Env
+  when 'development'
+    DataMapper::Logger.new(STDOUT, :debug)
+    DataMapper.setup(:default, "sqlite3:///#{Dir.pwd}/db/dev.db")
+  else
+    DataMapper::Logger.new(STDOUT, :warn)
+    DataMapper.setup(:default, "sqlite3:///#{Dir.pwd}/db/prod.db")
+  end
 end
